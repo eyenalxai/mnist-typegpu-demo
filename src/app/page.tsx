ab3951aaf8d0039d5b5957134dbdefbe354007d9
Sax Dhiut <mail@iyxeyl.me>
2025-11-30 19:15:30 +0400

    style: update main layout in page component to set maximum width for improved responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { MNISTCanvas } from "@/components/mnist-canvas"
 
 export default function Page() {
 	return (
-		<main className="container mx-auto py-8 px-4">
+		<main className="container mx-auto py-8 px-4 max-w-lg">
 			<MNISTCanvas />
 		</main>
 	)
